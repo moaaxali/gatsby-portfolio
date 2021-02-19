871bd6521ad723a0820d318564c2287e5ec0b8b6
Maaz Ali <moaaxali@gmail.com>
2021-02-19 20:52:46 +0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  siteMetadata: {
+    title: "proj_01",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-offline",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
