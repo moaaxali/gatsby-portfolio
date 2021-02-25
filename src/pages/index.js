49e82d785be03b9c568e8158d55d55121e721a14
Maaz Ali <moaaxali@gmail.com>
2021-02-25 19:54:00 +0500

    Add Link to gatsby index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from "react"
+import { Link } from "gatsby"
 
 // markup
 const IndexPage = () => {
   return (
     <main>
       <div style={{ color: `purple` }}>
+        <Link to="/contact/">Contact</Link>
         <h1>Welcome to Gatsby!</h1>
         <p>Continue... a happy learning with gatsby!!!</p>
         <img src="https://source.unsplash.com/random/400x200" alt="" />
