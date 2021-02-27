3e497e7b763b37d6b015b1ae3128a00df7a32240
Maaz Ali <moaaxali@gmail.com>
2021-02-27 21:29:11 +0500

    Import Layout in contact.js page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Header from "../components/header"
+import Layout from '../components/layout'
 
 export default function Contact() {
   return (
-    <div style={{ color: `teal` }}>
-      <Link to="/">Home</Link>
-      <Header headerText="Contact Me!" />
-      <p>
-        <a href="mailto:me@example.com">me@example.com</a>
-      </p>
-    </div>
+    <Layout>
+      <div style={{ color: `teal` }}>
+        <Link to="/">Home</Link>
+        <Header headerText="Contact Me!" />
+        <p>
+          <a href="mailto:me@example.com">me@example.com</a>
+        </p>
+      </div>
+    </Layout>
   )
 }
