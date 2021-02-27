f6086afb42a4face0b6e5abf88e6765006f7adbb
Maaz Ali <moaaxali@gmail.com>
2021-02-27 21:28:09 +0500

    Import Layout in about.js page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,15 @@
 import React from "react"
 import Header from "../components/header"
+import Layout from "../components/layout"
+
 
 export default function About() {
   return (
-    <div style={{ color: `teal` }}>
-      <Header headerText="About Me!" />
-      <p>A software engineer, a cyber security and cloud enthusiast, and a tech lover.</p>
-    </div>
+    <Layout>
+      <div style={{ color: `teal` }}>
+        <Header headerText="About Me!" />
+        <p>A software engineer, a cyber security and cloud enthusiast, and a tech lover.</p>
+      </div>
+    </Layout>
   )
 }
