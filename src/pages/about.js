e25b6a35be506ecf8710b16528650c2e39f48e3e
Maaz Ali <moaaxali@gmail.com>
2021-02-27 21:16:21 +0500

    Update about.js page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,8 +4,8 @@ import Header from "../components/header"
 export default function About() {
   return (
     <div style={{ color: `teal` }}>
-      <Header headerText="About Gatsby!" />
-      <p>Such wow. Very React.</p>
+      <Header headerText="About Me!" />
+      <p>A software engineer, a cyber security and cloud enthusiast, and a tech lover.</p>
     </div>
   )
 }
