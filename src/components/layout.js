d829009c0ca0269a893ae7450e72517a8aa5e912
Maaz Ali <moaaxali@gmail.com>
2021-02-27 21:37:08 +0500

    Clean index.js page a little

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import React from "react"
 export default function Layout({ children }) {
   return (
     <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+      <h1>@moaaxali</h1>
       {children}
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,7 @@ const IndexPage = () => {
   return (
     <Layout>
       <main>
-        <div style={{ color: `purple` }}>
-          <Link to="/contact/">Contact</Link>
-          <h1>Welcome to Gatsby!</h1>
-          <p>Continue... a happy learning with gatsby!!!</p>
-          <img src="https://source.unsplash.com/random/400x200" alt="" />
-        </div>
+
       </main >
     </Layout>
   )
