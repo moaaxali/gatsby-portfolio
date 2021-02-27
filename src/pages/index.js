6b90c09af82305cad04849951011ac16aa67fd5b
Maaz Ali <moaaxali@gmail.com>
2021-02-27 21:26:43 +0500

    Import Layout in index.js and wrap contents

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,20 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import Layout from "../components/layout"
 
 // markup
 const IndexPage = () => {
   return (
-    <main>
-      <div style={{ color: `purple` }}>
-        <Link to="/contact/">Contact</Link>
-        <h1>Welcome to Gatsby!</h1>
-        <p>Continue... a happy learning with gatsby!!!</p>
-        <img src="https://source.unsplash.com/random/400x200" alt="" />
-      </div>
-    </main >
+    <Layout>
+      <main>
+        <div style={{ color: `purple` }}>
+          <Link to="/contact/">Contact</Link>
+          <h1>Welcome to Gatsby!</h1>
+          <p>Continue... a happy learning with gatsby!!!</p>
+          <img src="https://source.unsplash.com/random/400x200" alt="" />
+        </div>
+      </main >
+    </Layout>
   )
 }
 
